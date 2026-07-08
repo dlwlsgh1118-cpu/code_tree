4df0059c18b9bdb1b48ca8b75350a73eb43db244
dlwlsgh1118-cpu <dlwlsgh1118@gmail.com>
2026-07-08 15:50:52 +0900

    [Passed] 두 정수에 대한 연산값 2 (54ms, 16MB)

---
+++ trail2/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def f(a,b):
+    if a > b:
+        return [a*2,b+10]
+    else:
+        return [a+10,b*2]
+
+a,b = f(a,b)
+print(a,b)
