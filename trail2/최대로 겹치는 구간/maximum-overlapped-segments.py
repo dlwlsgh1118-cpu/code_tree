f23f5dd365555538d545ea9297f418d3256a2de0
dlwlsgh1118-cpu <dlwlsgh1118@gmail.com>
2026-09-20 19:52:27 +0900

    [Passed] 최대로 겹치는 구간 (48ms, 16MB)

---
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,11 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+list = [0 for _ in range(200)]
+cnt = 0
+for i in range(n):
+    for j in range(segments[i][0],segments[i][1]):
+        list[j] += 1
+
+print(max(list))
